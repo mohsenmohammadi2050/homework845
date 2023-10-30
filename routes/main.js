9fbcf005ba32e09bfd4737b3c55537be72f691eb
Mohsen Mohammadi <mohsenmohammadi2050@gmail.com>
2023-10-30 19:14:43 +0100

    ss

--- routes/main.js
+++ routes/main.js
@@ -4,15 +4,15 @@ var save_content = require("../save_text")
 
 
 router.get("/", async function(req, res){
-    let text = await s3.getObject({
-        Bucket: "cyclic-outstanding-teal-gazelle-eu-west-3",
-        Key: "number.json",
-      }).then(res => {
-        console.log("from get ", res)
-      }).catch(err => {
-        console.log("from get err", err)
-      })
-    res.render("main", {text: text})
+    // let text = await s3.getObject({
+    //     Bucket: "cyclic-outstanding-teal-gazelle-eu-west-3",
+    //     Key: "number.json",
+    //   }).then(res => {
+    //     console.log("from get ", res)
+    //   }).catch(err => {
+    //     console.log("from get err", err)
+    //   })
+    res.render("main")
 })
 
 
